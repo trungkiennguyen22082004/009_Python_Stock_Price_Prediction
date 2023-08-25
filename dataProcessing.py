140bde3ca1681f17562a076b2477d2c3ec9507f5
moon22082004 <114036681+trungkiennguyen22082004@users.noreply.github.com>
2023-08-25 22:21:51 +1000

    Data processing 1

---
+++ dataProcessing.py
@@ -0,0 +1,118 @@
+import os
+
+import pandas as pd
+import numpy as np
+from tensorflow.python import train
+import yfinance as yf
+
+from sklearn.preprocessing import MinMaxScaler
+import joblib
+
+from parameters import COMPANY, DATA_SOURCE, END_DATE, START_DATE
+
+def processData(isStoredDataLocally=True, company="TSLA", startDate="2015-01-01", endDate="2020-01-01", dataSource = "yahoo", 
+                    trainRatio=0.8, randomSplit=False, randomSeed=None,
+                    isScaledData=True, featureRange=(0, 1), isStoredScaler=True):
+    #   Check if the stock data's folder is exist
+    if (not os.path.isdir("stockdatas")):
+        os.mkdir("stockdatas")    
+
+    # LOAD DATA
+    trainDataFilePath = "stockdatas/{}_{}.csv".format(company, "training")
+    testDataFilePath = "stockdatas/{}_{}.csv".format(company, "testing")
+
+    if (dataSource == "yahoo"):
+        # Check if the data has been saved
+        if (os.path.exists(trainDataFilePath) and (os.path.exists(testDataFilePath))):
+            trainData = pd.read_csv(trainDataFilePath, index_col=0, parse_dates=True)
+            testData = pd.read_csv(testDataFilePath, index_col=0, parse_dates=True)
+            print("Loaded datas from the saved file")
+        # If the data has not been saved
+        else:
+            # Fetch data from Yahoo Finance
+            data = yf.download(company, start=startDate, end=endDate, progress=False)
+            usedData = data.copy()                                             # Make a copy to avoid modifying the original data
+            # Handle NaN values in the data
+            usedData.fillna(method='ffill', inplace=True)                      # Forward fill missing values
+
+            # SPLIT DATA
+            (trainData, testData) = splitData(usedData, trainRatio, randomSplit, randomSeed)
+            print("Fetched data.")
+
+            # SAVE DATA
+            if (isStoredDataLocally):
+                trainData.to_csv(trainDataFilePath)
+                testData.to_csv(testDataFilePath)
+
+        # SCALE DATA 
+        if (isScaledData):
+            trainData = scaleData(company, trainData, "training", isStoredScaler, featureRange)
+            testData = scaleData(company, testData, "testing", isStoredScaler, featureRange)
+
+        return (trainData, testData)
+
+def splitData(data, trainRatio, randomSplit, randomSeed):
+    if (not 0 < trainRatio < 1):
+        raise ValueError("train ratio should be between 0 and 1.")    
+    
+    # Split randomly
+    if (randomSplit):
+        if (randomSeed is not None):
+            np.random.seed(randomSeed)
+        mask = np.random.rand(len(data)) < trainRatio
+        trainData = data[mask]
+        testData = data[~mask]
+    # Split by date: The training data comes from the earlier dates and the testing data comes from the later dates
+    else:
+        splitDate = int(len(data) * trainRatio)
+        trainData = data[:splitDate]
+        testData = data[splitDate:]
+    
+    return trainData, testData
+
+def scaleData(company, data, dataType, isStoredScaler, featureRange=(0, 1)):
+    # Check if the stock data's folder is exist
+    if (not os.path.isdir("stockdatas")):
+        os.mkdir("stockdatas")    
+
+    scalerFilePath = "stockdatas/scaler_{}_{}.pkl".format(company, dataType)
+    dataFilePath = "stockdatas/{}_{}.csv".format(company, dataType)
+    
+    # No data found
+    if (not os.path.exists(dataFilePath)):
+        print("The {} data is missing.".format(dataType))
+    # Scale the data with the new scaler
+    elif (not os.path.exists(scalerFilePath)):
+        # Scale data
+        scaler = MinMaxScaler(feature_range=featureRange)
+        scaledData = scaler.fit_transform(data)
+        print("Scaled {} data.".format(dataType))
+        
+        # Save scaler
+        if (isStoredScaler):
+            joblib.dump(scaler, scalerFilePath)
+            print("Saved {} scaler.".format(dataType))
+    # Load scaler and scaled data
+    else:
+        data = pd.read_csv(dataFilePath, index_col=0, parse_dates=True)
+        scaler = joblib.load(scalerFilePath)
+        print("Loaded {} scaler.".format(dataType))
+        
+        scaledData = scaler.transform(data.copy())  
+        print("Scaled {} data.".format(dataType))
+
+    return scaledData, scaler
+
+# TEST DATA PROCESSING
+
+# (trainData, testData) = processData(True, COMPANY, START_DATE, END_DATE, DATA_SOURCE, 0.8, False, None, True, (0, 1), True)
+
+(trainData, testData) = processData(True, COMPANY, START_DATE, END_DATE, DATA_SOURCE, 0.8, False, None, False, (0, 1), False)
+
+print("===============================================================")
+print("Train Data:")
+print(trainData)
+
+print("===============================================================")
+print("Test Data")
+print(testData)
--- stock_prediction.py
+++ stock_prediction.py
@@ -8,13 +8,10 @@
 # By: NeuralNine
 
 # Need to install the following:
-# pip install numpy
-# pip install matplotlib
-# pip install pandas
-# pip install tensorflow
-# pip install scikit-learn
-# pip install pandas-datareader
-# pip install yfinance
+# pip install numpy matplotlib pandas tensorflow scikit-learn pandas-datareader yfinance
+
+from genericpath import exists
+import os
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -25,27 +22,29 @@ import tensorflow as tf
 import yfinance as yf
 
 from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, LSTM, InputLayer
+from sklearn.externals import joblib
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, LSTM, InputLayer
+
+from parameters import *
+from dataProcessing import *
 
 #------------------------------------------------------------------------------
 # Load Data
 ## TO DO:
 # 1) Check if data has been saved before. 
-# If so, load the saved data
-# If not, save the data into a directory
+#   If so, load the saved data
+#   If not, save the data into a directory
 #------------------------------------------------------------------------------
-DATA_SOURCE = "yahoo"
-COMPANY = "TSLA"
+
+# DATA_SOURCE = "yahoo"
+# COMPANY = "TSLA"
 
 # start = '2012-01-01', end='2017-01-01'
-# TRAIN_START = '2015-01-01'
-# TRAIN_END = '2020-01-01'
+# START_DATE = '2015-01-01'
+# END_DATE = '2020-01-01'
 
-TRAIN_START = '2012-01-01'
-TRAIN_END = '2017-01-01'
-
-data =  yf.download(COMPANY, start=TRAIN_START, end=TRAIN_END, progress=False)
+data = yf.download(COMPANY, start=START_DATE, end=END_DATE, progress=False)
 # yf.download(COMPANY, start = TRAIN_START, end=TRAIN_END)
 
 # For more details: 
@@ -54,30 +53,24 @@ data =  yf.download(COMPANY, start=TRAIN_START, end=TRAIN_END, progress=False)
 # Prepare Data
 ## To do:
 # 1) Check if data has been prepared before. 
-# If so, load the saved data
-# If not, save the data into a directory
+#   If so, load the saved data
+#   If not, save the data into a directory
 # 2) Use a different price value eg. mid-point of Open & Close
 # 3) Change the Prediction days
 #------------------------------------------------------------------------------
 PRICE_VALUE = "Close"
 
 scaler = MinMaxScaler(feature_range=(0, 1)) 
-# Note that, by default, feature_range=(0, 1). Thus, if you want a different 
-# feature_range (min,max) then you'll need to specify it here
+# Note that, by default, feature_range=(0, 1). Thus, if you want a different feature_range (min,max) then you'll need to specify it here
 scaled_data = scaler.fit_transform(data[PRICE_VALUE].values.reshape(-1, 1)) 
 # Flatten and normalise the data
-# First, we reshape a 1D array(n) to 2D array(n,1)
-# We have to do that because sklearn.preprocessing.fit_transform()
-# requires a 2D array
+# First, we reshape a 1D array(n) to 2D array(n,1). We have to do that because sklearn.preprocessing.fit_transform() requires a 2D array
 # Here n == len(scaled_data)
 # Then, we scale the whole array to the range (0,1)
-# The parameter -1 allows (np.)reshape to figure out the array size n automatically 
-# values.reshape(-1, 1) 
+# The parameter -1 allows (np.) reshape to figure out the array size n automatically values.reshape(-1, 1) 
 # https://stackoverflow.com/questions/18691084/what-does-1-mean-in-numpy-reshape'
-# When reshaping an array, the new shape must contain the same number of elements 
-# as the old shape, meaning the products of the two shapes' dimensions must be equal. 
-# When using a -1, the dimension corresponding to the -1 will be the product of 
-# the dimensions of the original array divided by the product of the dimensions 
+# When reshaping an array, the new shape must contain the same number of elements as the old shape, meaning the products of the two shapes' dimensions must be equal. 
+# When using a -1, the dimension corresponding to the -1 will be the product of the dimensions of the original array divided by the product of the dimensions 
 # given to reshape so as to maintain the same number of elements.
 
 # Number of days to look back to base the prediction
@@ -95,47 +88,39 @@ for x in range(PREDICTION_DAYS, len(scaled_data)):
 
 # Convert them into an array
 x_train, y_train = np.array(x_train), np.array(y_train)
-# Now, x_train is a 2D array(p,q) where p = len(scaled_data) - PREDICTION_DAYS
-# and q = PREDICTION_DAYS; while y_train is a 1D array(p)
+# Now, x_train is a 2D array(p,q) where p = len(scaled_data) - PREDICTION_DAYS and q = PREDICTION_DAYS; while y_train is a 1D array(p)
 
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-# We now reshape x_train into a 3D array(p, q, 1); Note that x_train 
-# is an array of p inputs with each input being a 2D array 
+# We now reshape x_train into a 3D array(p, q, 1); Note that x_train is an array of p inputs with each input being a 2D array 
 
 #------------------------------------------------------------------------------
 # Build the Model
 ## TO DO:
 # 1) Check if data has been built before. 
-# If so, load the saved data
-# If not, save the data into a directory
+#   If so, load the saved data
+#   If not, save the data into a directory
 # 2) Change the model to increase accuracy?
 #------------------------------------------------------------------------------
 model = Sequential() # Basic neural network
-# See: https://www.tensorflow.org/api_docs/python/tf/keras/Sequential
-# for some useful examples
+# See: https://www.tensorflow.org/api_docs/python/tf/keras/Sequential for some useful examples
 
 model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
 # This is our first hidden layer which also spcifies an input layer. 
-# That's why we specify the input shape for this layer; 
-# i.e. the format of each training example
+# That's why we specify the input shape for this layer; i.e. the format of each training example
 # The above would be equivalent to the following two lines of code:
 # model.add(InputLayer(input_shape=(x_train.shape[1], 1)))
 # model.add(LSTM(units=50, return_sequences=True))
 # For som eadvances explanation of return_sequences:
 # https://machinelearningmastery.com/return-sequences-and-return-states-for-lstms-in-keras/
 # https://www.dlology.com/blog/how-to-use-return_state-or-return_sequences-in-keras/
-# As explained there, for a stacked LSTM, you must set return_sequences=True 
-# when stacking LSTM layers so that the next LSTM layer has a 
-# three-dimensional sequence input. 
+# As explained there, for a stacked LSTM, you must set return_sequences=True when stacking LSTM layers so that the next LSTM layer has a three-dimensional sequence input. 
 
 # Finally, units specifies the number of nodes in this layer.
-# This is one of the parameters you want to play with to see what number
-# of units will give you better prediction quality (for your problem)
+# This is one of the parameters you want to play with to see what number of units will give you better prediction quality (for your problem)
 
 model.add(Dropout(0.2))
-# The Dropout layer randomly sets input units to 0 with a frequency of 
-# rate (= 0.2 above) at each step during training time, which helps 
-# prevent overfitting (one of the major problems of ML). 
+# The Dropout layer randomly sets input units to 0 with a frequency of rate (= 0.2 above) at each step during training time, which helps prevent overfitting 
+# (one of the major problems of ML). 
 
 model.add(LSTM(units=50, return_sequences=True))
 # More on Stacked LSTM:
@@ -151,36 +136,24 @@ model.add(Dense(units=1))
 # We compile the model by specify the parameters for the model
 # See lecture Week 6 (COS30018)
 model.compile(optimizer='adam', loss='mean_squared_error')
-# The optimizer and loss are two important parameters when building an 
-# ANN model. Choosing a different optimizer/loss can affect the prediction
-# quality significantly. You should try other settings to learn; e.g.
-    
+# The optimizer and loss are two important parameters when building an ANN model. Choosing a different optimizer/loss can affect the prediction quality significantly. 
+# You should try other settings to learn; e.g.   
 # optimizer='rmsprop'/'sgd'/'adadelta'/...
 # loss='mean_absolute_error'/'huber_loss'/'cosine_similarity'/...
 
-# Now we are going to train this model with our training data 
-# (x_train, y_train)
+# Now we are going to train this model with our training data (x_train, y_train)
 model.fit(x_train, y_train, epochs=25, batch_size=32)
-# Other parameters to consider: How many rounds(epochs) are we going to 
-# train our model? Typically, the more the better, but be careful about
-# overfitting!
-# What about batch_size? Well, again, please refer to 
-# Lecture Week 6 (COS30018): If you update your model for each and every 
-# input sample, then there are potentially 2 issues: 1. If you training 
-# data is very big (billions of input samples) then it will take VERY long;
-# 2. Each and every input can immediately makes changes to your model
-# (a souce of overfitting). Thus, we do this in batches: We'll look at
-# the aggreated errors/losses from a batch of, say, 32 input samples
-# and update our model based on this aggregated loss.
+# Other parameters to consider: How many rounds(epochs) are we going to train our model? Typically, the more the better, but be careful about overfitting!
+# What about batch_size? Well, again, please refer to Lecture Week 6 (COS30018): If you update your model for each and every input sample, then there are potentially 2 issues: 
+#   1. If you training data is very big (billions of input samples) then it will take VERY long;
+#   2. Each and every input can immediately makes changes to your model (a souce of overfitting). 
+# Thus, we do this in batches: We'll look at the aggreated errors/losses from a batch of, say, 32 input samples and update our model based on this aggregated loss.
 
 # TO DO:
 # Save the model and reload it
-# Sometimes, it takes a lot of effort to train your model (again, look at
-# a training data with billions of input samples). Thus, after spending so 
-# much computing power to train your model, you may want to save it so that
-# in the future, when you want to make the prediction, you only need to load
-# your pre-trained model and run it on the new input for which the prediction
-# need to be made.
+# Sometimes, it takes a lot of effort to train your model (again, look at a training data with billions of input samples). Thus, after spending so 
+# much computing power to train your model, you may want to save it so that in the future, when you want to make the prediction, you only need to load
+# your pre-trained model and run it on the new input for which the prediction need to be made.
 
 #------------------------------------------------------------------------------
 # Test the model accuracy on existing data
@@ -189,7 +162,7 @@ model.fit(x_train, y_train, epochs=25, batch_size=32)
 TEST_START = '2020-01-02'
 TEST_END = '2022-12-31'
 
-test_data = yf.download(COMPANY, start=TRAIN_START, end=TRAIN_END, progress=False)
+test_data = yf.download(COMPANY, start=START_DATE, end=END_DATE, progress=False)
 
 # The above bug is the reason for the following line of code
 test_data = test_data[1:]
@@ -199,26 +172,18 @@ actual_prices = test_data[PRICE_VALUE].values
 total_dataset = pd.concat((data[PRICE_VALUE], test_data[PRICE_VALUE]), axis=0)
 
 model_inputs = total_dataset[len(total_dataset) - len(test_data) - PREDICTION_DAYS:].values
-# We need to do the above because to predict the closing price of the fisrt
-# PREDICTION_DAYS of the test period [TEST_START, TEST_END], we'll need the 
-# data from the training period
+# We need to do the above because to predict the closing price of the first PREDICTION_DAYS of the test period [TEST_START, TEST_END], we'll need the data from the training period
 
 model_inputs = model_inputs.reshape(-1, 1)
 # TO DO: Explain the above line
 
 model_inputs = scaler.transform(model_inputs)
-# We again normalize our closing price data to fit them into the range (0,1)
-# using the same scaler used above 
-# However, there may be a problem: scaler was computed on the basis of
-# the Max/Min of the stock price for the period [TRAIN_START, TRAIN_END],
-# but there may be a lower/higher price during the test period 
-# [TEST_START, TEST_END]. That can lead to out-of-bound values (negative and
-# greater than one)
+# We again normalize our closing price data to fit them into the range (0,1) using the same scaler used above.
+# However, there may be a problem: scaler was computed on the basis of the Max/Min of the stock price for the period [TRAIN_START, TRAIN_END], but there may be a lower/higher price during the test period 
+# [TEST_START, TEST_END]. That can lead to out-of-bound values (negative and greater than one)
 # We'll call this ISSUE #2
 
-# TO DO: Generally, there is a better way to process the data so that we 
-# can use part of it for training and the rest for testing. You need to 
-# implement such a way
+# TO DO: Generally, there is a better way to process the data so that we can use part of it for training and the rest for testing. You need to implement such a way
 
 #------------------------------------------------------------------------------
 # Make predictions on test data
@@ -233,8 +198,7 @@ x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 
 predicted_prices = model.predict(x_test)
 predicted_prices = scaler.inverse_transform(predicted_prices)
-# Clearly, as we transform our data into the normalized range (0,1),
-# we now need to reverse this transformation 
+# Clearly, as we transform our data into the normalized range (0,1), we now need to reverse this transformation 
 #------------------------------------------------------------------------------
 # Plot the test predictions
 ## To do:
@@ -267,16 +231,9 @@ prediction = scaler.inverse_transform(prediction)
 print(f"Prediction: {prediction}")
 
 # A few concluding remarks here:
-# 1. The predictor is quite bad, especially if you look at the next day 
-# prediction, it missed the actual price by about 10%-13%
-# Can you find the reason?
-# 2. The code base at
-# https://github.com/x4nth055/pythoncode-tutorials/tree/master/machine-learning/stock-prediction
-# gives a much better prediction. Even though on the surface, it didn't seem 
-# to be a big difference (both use Stacked LSTM)
-# Again, can you explain it?
-# A more advanced and quite different technique use CNN to analyse the images
-# of the stock price changes to detect some patterns with the trend of
-# the stock price:
+#   1. The predictor is quite bad, especially if you look at the next day  prediction, it missed the actual price by about 10%-13%. Can you find the reason?
+#   2. The code base at https://github.com/x4nth055/pythoncode-tutorials/tree/master/machine-learning/stock-prediction gives a much better prediction. Even though on the surface, it didn't seem 
+# to be a big difference (both use Stacked LSTM). Again, can you explain it?
+# A more advanced and quite different technique use CNN to analyse the images of the stock price changes to detect some patterns with the trend of the stock price:
 # https://github.com/jason887/Using-Deep-Learning-Neural-Networks-and-Candlestick-Chart-Representation-to-Predict-Stock-Market
 # Can you combine these different techniques for a better prediction??
